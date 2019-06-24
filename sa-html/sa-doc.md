ee36d4fb5317d315dab18b5055258ac2bad9dbad
孔 <2393584716@qq.com>
2019-06-24 22:26:51 +0800

    修复bug：鼠标悬浮page-title时，偶尔动画混乱的bug 修复bug：优化折叠动画，更流畅了 集成swiper，五种高大上窗口切换动画

---
+++ sa-html/sa-doc.md
@@ -0,0 +1,75 @@
+# sa-admin 使用步骤
+
+&emsp; 五分钟快速上手
+
+#### 1、获取 源码
+> - 你有两种方式获取源码
+> - 通过gitee、或github获取源码
+> - [点我直接下载](http://sa-admin.dev33.cn/sa-admin-dev.rar)
+
+#### 2、使用说明
+> - 在使用时，不建议你直接魔改模板的代码，以免在运行时出现意外bug，而是用扩展的方法，来适应你的业务逻辑
+> - 如何扩展？在sa-resourecs文件下，有个 my-code.js ，这是专门为了方便你接入你的业务逻辑而预留的一个文件 你可以在此文件中根据模板提供的API来操作模板
+> - 具体可以操作哪些接口？接着往下看
+
+
+
+
+#### 3、设置模板标题
+``` js
+sp.title = "SA-后台模板";
+```
+
+#### 4、自定义菜单树
+``` js 
+var myMenuList = window.menuList;	// window.menuList 在 menu-list.js 中定义 
+sp.setMenuList(myMenuList);	// 写入菜单 
+// sp.setMenuList(myMenuList, [11, 1, '1-1']);	// 写入菜单，并设置应该显示哪些id的菜单（第二个参数为空时，代表默认显示所有）
+```
+
+#### 5、js控制打开某个菜单
+```js
+sp.showMenuById('1-1');	// 打开一个 菜单，根据 id 
+sp.closePageById('1-1');	// 关闭一个 页面，根据 id 
+// 打开一个自定义 页面 
+// sp.showPage({id: 12345, name: '新页面', url: 'http://web.yanzhi21.com'});	// id尽量不要和已有的菜单id冲突，其它属性均可参照菜单项
+```
+
+#### 6、如何设置登录后右上角显示的user信息
+``` js
+sp.user = { 
+    username: 'root', // 昵称	
+    avatar: 'sa-resources/admin-logo.png' // 头像地址 
+}
+```
+
+#### 7、重写按钮事件
+你可以轻松自定义登录后的头像处，下拉可以出现的选项
+``` js
+sp.dropList = [	// 头像点击处可操作的选项	
+    {	
+    	name: '我的资料',	
+    	click: function() {	
+        	/* balabala... */
+        }	
+    },	
+    {	
+        name: '退出登录',	
+        click: function() {
+            	/* balabala... */
+        }	
+    }	
+]	
+``` 
+
+#### 8、以上示例在 my-code.js中 都有相应的注释说明，如何还有不懂的地方，可以加群问我（群链接在首页）
+
+
+
+
+
+
+
+
+
+
--- sa-resources/index.js
+++ sa-resources/index.js
@@ -1,3 +1,4 @@
+// 首页 
 var homePage = {
 	id: '-1',	// 唯一标识 
 	name: '首页',
@@ -8,20 +9,31 @@ var homePage = {
 var sp = new Vue({
 	el: '.app',
 	data: {
-		version: 'v1.0.8',		// 当前版本
+		version: 'v1.1.0',		// 当前版本
+		update_time: '2019-6-24',		// 更新日期 
 		title: 'SA-后台模板',				// 页面标题  
+		logo_url: 'sa-resources/admin-logo.png',	// logo地址 
 		default_active: '0',	// 默认的高亮菜单id
 		menuList: [],		// 菜单集合 
-		pageList: [homePage],	// 页面集合
 		nativePage: homePage,	// 当前正显示的Page 
+		pageList: [homePage],	// 页面集合
 		scrollX: 0		,// 滚动条位置 
-		rightPage: null,	// 右键操作的page
 		rightShow: false,	// 右键菜单是否显示 
+		rightPage: null,	// 右键正在操作的page
 		rightZB: {x: 0, y: 0}	,// 右键菜单坐标
 		is_fold: false,			// 菜单是否折叠
-		is_full_screen: false	,// 是否全屏
+		is_fold_right: false,	// 右边是否折叠（将右边盒子折叠与菜单折叠分开，这样可以减少动画的卡顿现象）   
+		is_full_screen: false	,// 是否全屏   
 		user: null	,// user信息
 		now_time: '加载中...'	,// 当前时间 
+		switchV: localStorage.getItem('switchV') || 'slide',	// 切换效果 
+		switchList: [	// 切换动画数组 
+			{name: '滑动', value: 'slide'},
+			{name: '淡入', value: 'fade'},
+			{name: '方块', value: 'cube'},
+			{name: '3D流', value: 'coverflow'},
+			{name: '3D翻转', value: 'flip'}
+		],
 		themeV: localStorage.getItem('themeV') || '1',	// 当前主题值
 		themeList: [	// 主题数组
 			{name: '主题 1', value: '1'},
@@ -29,7 +41,7 @@ var sp = new Vue({
 			{name: '主题 3', value: '3'},
 			{name: '主题 4', value: '4'}
 		],
-		dropList: []
+		dropList: []	// 头像处下拉列表菜单 
 	},
 	watch: {
 		// 监听全屏动作 
@@ -47,8 +59,8 @@ var sp = new Vue({
 	},
 	methods: {
 		// ------------------- 对外预留接口 --------------------
-		// 写入菜单，可以是一个一维数组(指定好parent_id)，也可以是一个已经渲染好的tree数组
-		// show_list 为指定显示的id集合(注意是id的集合)，为空时代表显示所有
+		// 写入菜单，可以是一个一维数组(指定好parent_id)，也可以是一个已经渲染好的tree数组	
+		// show_list 为指定显示的id集合(注意是id的集合)，为空时代表显示所有	
 		setMenuList: function(menu_list, show_list) {
 			// 转化为string 便于比较
 			if(show_list) {
@@ -127,6 +139,22 @@ var sp = new Vue({
 				}
 			}
 		},
+		// 切换翻页方式
+		toggleSwitch: function(command) {
+			this.switchV = command + "";
+			localStorage.setItem('switchV', command);
+			
+			this.$confirm('此动画效果将在您刷新页面之后生效，是否立即刷新？', {
+				confirmButtonText: '确定',
+				cancelButtonText: '取消',
+				type: 'warning'
+			}).then(function() {
+				location.reload();
+			}).catch(function() {
+				
+			});
+			
+		},
 		// 处理userinfo的下拉点击
 		handleCommand: function(command) {
 			this.dropList.forEach(function(drop) {
@@ -138,6 +166,29 @@ var sp = new Vue({
 		// 退出登录
 		login_out: function() {
 			console.log('退出登录');
+		},
+		// 折叠菜单
+		fold_start: function() {
+			this.is_fold_right = true; 
+			sw.updateSlideSize(100);	// swipre重新计算大小  
+			// 如果打开的 iframe 在五个以内  浏览器压力很小 就立刻展开菜单，
+			// 如果打开 iframe 超过5个，浏览器就比较有压力， 此时会卡顿短暂时间，此时延时折叠菜单，让动画显得没那么卡 
+			if(this.pageList.length <= 5) {
+				this.is_fold = true;
+			} else {
+				setTimeout(function() {
+					this.is_fold = true;
+				}.bind(this), 100);
+			}
+		},
+		// 展开菜单
+		fold_end: function() {
+			this.is_fold = false;
+			// 延时200ms执行，让它没那么卡 
+			setTimeout(function() {
+				this.is_fold_right = false; 
+				sw.updateSlideSize();	// swipre重新计算大小  
+			}.bind(this), 200);
 		},
 		// ------------------- p-title右键菜单相关 --------------------
 		// 右键 p-title
@@ -236,15 +287,22 @@ var sp = new Vue({
 		// 关闭页面
 		closePage: function(page) {
 			if(page == this.nativePage) {
-				var index = this.pageList.indexOf(page);
-				var prePage = this.pageList[index - 1];
-				this.showPage(prePage);
+				var index = this.pageList.indexOf(page); 
+				var prePage = this.pageList[index - 1]; 
+				this.showPage(prePage); 
 			}
+			// 删除 slide 
+			var index = this.pageList.indexOf(page);
+			sw.mySwiper.removeSlide(index);
 			arrayDelete(this.pageList, page);
 		},
 		// 添加一个Page
 		addPage: function(page) {
 			this.pageList.push(page);
+			var slide = '<div class="swiper-slide">' + 
+						'	<iframe src="' + page.url + '" id="iframe' + page.id + '" class="iframe"></iframe>' + 
+						'</div>';
+			sw.mySwiper.appendSlide(slide);
 		},
 		// 显示某个页面, 
 		// page对象，是否强制刷新 
@@ -259,6 +317,11 @@ var sp = new Vue({
 			}
 			this.nativePage = page;
 			this.default_active = page.id + '';	// 左边自动关联, 如果左边没有，则无效果 
+			
+			// swiper切换
+			var index = this.pageList.indexOf(page);
+			sw.slideTo(index);
+			
 			// 归位一下
 			this.$nextTick(function() {
 				this.scrollToAuto();	
@@ -319,15 +382,77 @@ var sp = new Vue({
 	},
 	created:function(){
 		
-		// 调整是否收拾
-		if(document.body.clientWidth < 800) {
-			this.is_fold = true;
-		} else {
-			this.is_fold = false;
-		}
+		setTimeout(function() {
+			console.log('欢迎使用sa-admin，当前版本：' + this.version + '，如在使用中发现任何bug或者疑问，请加群交流：782974737，点击加入：' + 'https://jq.qq.com/?_wv=1027&k=5DHN5Ib');
+		}.bind(this), 2000)
+		
+		// 调整是否
+		setTimeout(function() {
+			window.onresize();
+		}, 0);
 
 	}
+});
+
+
+
+// swiper相关方法 
+var mySwiper = new Swiper('.swiper-container', {
+	autoplay: false,		//可选选项，自动滑动
+	effect: sp.switchV
 })
+
+var sw = {
+	mySwiper: mySwiper,
+	// 转到指定slide
+	slideTo: function(index) {
+		// 先屏蔽掉所有滚动条
+		// document.querySelectorAll('.iframe').forEach(function(item) {
+		// 	item.classList.add('iframe-no-scroll');
+		// })
+		// 切换到第一个slide，速度为1秒
+		sw.mySwiper.slideTo(index, 300);
+		
+		
+		// 再打开所有滚动条
+		// setTimeout(function() {
+		// 	document.querySelectorAll('.iframe').forEach(function(item) {
+		// 		item.classList.remove('iframe-no-scroll');
+		// 		// arrayDelete(item.classList, 'iframe-no-scroll')
+		// 	})
+		// }, 200);
+	},
+	// 更正slide大小 ms = 延时毫秒数
+	updateSlideSize: function(ms) {
+		ms = ms || 1;
+		setTimeout(function() {
+			sw.mySwiper.update();	// swipre重新计算大小  
+		}, ms);
+		// var width = document.querySelector('.nav-right-3').style.width;
+		// document.querySelectorAll('.swiper-slide').forEach(function(item) {
+		// 	item.style.width = width;
+		// });
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+// 监听窗口大小变动
+window.onresize = function() {
+	if(document.body.clientWidth < 800) {
+		sp.fold_start();
+	} else {
+		sp.fold_end();
+	}
+}
 
 
 // 一直更新时间
@@ -343,15 +468,15 @@ setInterval(function() {
 	}
 	if (h >= 12) {
 		sx = "下午";
+		if (h >= 18) {
+			sx = "晚上";
+		}
 		h -= 12;
-	}
-	if (h >= 18) {
-		sx = "晚上";
 	}
 	var m = da.getMinutes(); //分
 	var s = da.getSeconds(); //秒
-	var z = da.getDay(); //周几
-	z = z == 0 ? '日' : z;
+	var z = ['日', '一', '二', '三', '四', '五', '六'][da.getDay()] ; //周几
+	// z = z == 0 ? '日' : z;
 	var zong = "";
 
 	zong += Y + "-" + M + "-" + D + " " + sx + " " + h + ":" + m + ":" + s + " 周" + z;
@@ -359,11 +484,3 @@ setInterval(function() {
 }, 1000)
 
 
-// 监听窗口大小变动
-window.onresize = function() {
-	if(document.body.clientWidth < 800) {
-		sp.is_fold = true;
-	} else {
-		sp.is_fold = false;
-	}
-}
